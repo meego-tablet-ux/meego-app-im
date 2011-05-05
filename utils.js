47ec3739c5656d4106efa6a93343d161f0bb5714
Jeremy Whiting <jpwhiting@kde.org>
2011-05-05 11:04:42 -0600

    Remove "Duration -" from duration string. Remove leading zero on hour in duration string.

--- utils.js
+++ utils.js
@@ -26,7 +26,7 @@ function getCallStatusText (agent) {
         break;
     case CallAgent.CallStatusTalking:
         // Qt has no way to get the time separator
-        return qsTr("Duration - ") + Qt.formatTime(window.callAgent.elapsedTime(), "HH:mm:ss");
+        return Qt.formatTime(window.callAgent.elapsedTime(), "h:mm:ss");
         break;
     case CallAgent.CallStatusHeld:
         return qsTr("On Hold");

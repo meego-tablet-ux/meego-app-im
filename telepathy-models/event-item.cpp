15a728841f5cff7cc50582c05d39796e50949ac3
Alvaro Soliverez <alvaro.soliverez@collabora.co.uk>
2011-08-23 08:59:18 -0700

    Fixed copyright assignment and license

--- telepathy-models/event-item.cpp
+++ telepathy-models/event-item.cpp
@@ -1,21 +1,9 @@
 /*
- * This file is part of TelepathyQt4Yell Models
+ * Copyright 2011 Intel Corporation.
  *
- * Copyright (C) 2010 Collabora Ltd. <http://www.collabora.co.uk/>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * This program is licensed under the terms and conditions of the
+ * Apache License, version 2.0.  The full text of the Apache License is at
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 #include "event-item.h"
--- telepathy-models/logger-conversation-model.cpp
+++ telepathy-models/logger-conversation-model.cpp
@@ -1,20 +1,9 @@
 /*
- * This file is part of TelepathyLoggerQt4
+ * Copyright 2011 Intel Corporation.
  *
- * Copyright (C) 2011 Collabora Ltd. <http://www.collabora.co.uk/>
- *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is licensed under the terms and conditions of the
+ * Apache License, version 2.0.  The full text of the Apache License is at
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 #include "logger-conversation-model.h"
